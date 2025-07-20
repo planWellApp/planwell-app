37750a80398da45a59b05dc6178dda8d9771190a
Aayush Sharma <aayush.sharma.as95@gmail.com>
2025-07-20 14:28:19 +0530

    firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCk2Oe6MO_A5CTu73Cz-FIFm2v8wBDuxbA",
+    authDomain: "planwell-7c968.firebaseapp.com",
+    projectId: "planwell-7c968",
+    storageBucket: "planwell-7c968.appspot.com",
+    messagingSenderId: "335597410510",
+    appId: "1:335597410510:web:ecac58de9a8ebf024a589f"
+  }
+};
